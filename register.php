1e0a9a4bbd758bc72ae489b60f81f3ffe8bf3c63
AndrewWang <yanroo8444@gmail.com>
2015-12-01 23:54:06 +0800

    able to register

---
+++ register.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Register</title>
+<style type="text/css">
+#rr{
+    border:2px solid black;
+    padding:10px;
+    background-color:white;
+    width:250px;
+    margin:10px auto;
+    font-size:20px;
+    font-family:fantasy;
+}    
+body{
+    background-color:#EEEEE0;
+    font-size:18px;
+}
+</style>
+</head>
+<body>
+</br>
+</br>
+</br>
+</br>
+</br>
+</br>
+</br>
+<h1 style="text-align:center;">註冊新帳號 </h1>
+
+<form method="post" action="insertuser.php">
+<div id="rr" align="center">
+帳號: <input type="text" name="id"><br />
+密碼: <input type="password" name="pwd"><br />
+<input type="submit">
+<input type="reset">
+</div>
+</form>
+</body>
+</html>
