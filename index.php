5dee9a2d99f05048b51d31ffa04490c01882b792
AndrewWang <yanroo8444@gmail.com>
2015-12-05 21:21:29 +0800

    add style

--- index.php
+++ index.php
@@ -1,42 +1,42 @@
-
 <!DOCTYPE html>
 <html>
 <head>
-<title>登入介面</title>
-<style type="text/css">
-#rr{
-    border:2px solid black;
-    padding:10px;
-    background-color:white;
-    width:250px;
-    margin:10px auto;
-    font-size:20px;
-    font-family:fantasy;
-}    
-body{
-    background-color:#EEEEE0;
-    font-size:18px;
-}
-</style>
+    <meta charset="UTF-8" />
+    <title>Welcome to Happy Farm</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-<meta charset="UTF-8" />
-</br>
-</br>
-</br>
-</br>
-</br>
-</br>
-</br>
-<h1 style="text-align:center;">Login Form</h1>
-<form method="post" action="login.php">
-<div id="rr" align="center">
-帳號: <input type="text" name="id"><br />
-密碼: <input type="password" name="pwd"><br />
-<input type="submit">
-<input type="reset">
-<a href="register.php" style="text-decoration:none; font-size:15px;">加入會員</a>
-</div>
-</form>
+    <div class="wrapper">
+        <div class="container">
+            <h1>Welcome to Happy Farm, Please Login</h1>
+            <form class="form" method="post" action="login.php">
+                <input type="text" placeholder="Username" name="id">
+                <input type="password" placeholder="Password" name="pwd">
+                <button type="submit" id="login-button">Login</button>
+                </br>
+                </br>
+                <a href="register.php" style="text-decoration:none; font-size:15px;">No account? Register now!</a>
+            </form>
+        </div>
+        <ul class="bg-bubbles">
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+    </div>
+    <!--<script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+    <script src="js/style.js"></script>-->
 </body>
 </html>
