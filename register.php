692c97219149d634a62e84bf1287825c86191d22
AndrewWang <yanroo8444@gmail.com>
2015-12-05 21:22:49 +0800

    add style

--- register.php
+++ register.php
@@ -1,42 +1,41 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Register</title>
-<style type="text/css">
-#rr{
-    border:2px solid black;
-    padding:10px;
-    background-color:white;
-    width:250px;
-    margin:10px auto;
-    font-size:20px;
-    font-family:fantasy;
-}    
-body{
-    background-color:#EEEEE0;
-    font-size:18px;
-}
-</style>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Register</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-</br>
-</br>
-</br>
-</br>
-</br>
-</br>
-</br>
-<h1 style="text-align:center;">註冊新帳號 </h1>
-
-<form method="post" action="insertuser.php">
-<div id="rr" align="center">
-帳號: <input type="text" name="id"><br />
-密碼: <input type="password" name="pwd"><br />
-姓名: <input type="text" name="pname"><br />
-<input type="submit">
-<input type="reset">
-</div>
-</form>
+    <div class="wrapper">
+        <div class="container">
+            <h1>註冊新帳號 </h1>
+            <form method="post" action="insertuser.php">
+            <!--輸入註冊的帳密，名字-->
+                <input type="text" placeholder="Username" name="id">
+                <input type="password" placeholder="Password" name="pwd">
+                <input type="text" placeholder="Name" name="pname">
+                <button type="submit" id="login-button">Register</button>
+            </form>
+        </div>
+        <ul class="bg-bubbles">
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+    </div>
+    <!--<script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+    <script src="js/style.js"></script>-->
 </body>
 </html>
